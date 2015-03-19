bf2f484d690833a9071f1724b2371660831288f2
Grigoriy Ostrovskiy <greevex@gmail.com>
2015-03-19 14:37:24 +0300

    Setup upgrade

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ echo "Installing ius-release repo..."
 yum install -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-13.ius.centos6.noarch.rpm
 echo "Creating Google Chrome repo file" 
 cp -f repo/google-chrome.repo /etc/yum.repos.d/
-echo "Installing Google Chrome
+echo "Installing Google Chrome..."
 chmod +x scripts/install_chrome.sh
-./scripts/install_chrome.sh -qq
-echo "Installing PHP5.5 ZTS"
-yum install -y php55t*
-echo "Installing memcached server"
+./scripts/install_chrome.sh -q
+echo "Installing PHP5.5 ZTS..."
+yum install -y php55t php55t-dba php55t-soap php55t-recode php55t-xmlrpc php55t-common php55t-pgsql php55t-opcache php55t-pear php55t-pecl-xdebug php55t-fpm php55t-pecl-raphf php55t-mcrypt php55t-litespeed php55t-pecl-propro php55t-pecl-jsonc php55t-imap php55t-pecl-pthreads php55t-gmp php55t-process php55t-interbase php55t-pecl-mongo php55t-intl php55t-embedded php55t-pdo php55t-pecl-gearman php55t-mysqlnd php55t-pecl-redis php55t-pecl-apcu php55t-pecl-memcached php55t-pspell php55t-pecl-ssh2 php55t-mbstring php55t-xml php55t-mssql php55t-pecl-pecl_http php55t-tidy php55t-snmp php55t-pecl-amqp php55t-enchant php55t-bcmath php55t-cli php55t-gd php55t-odbc php55t-pecl-opengl php55t-pecl-lzf php55t-ldap
+echo "Installing memcached server..."
 yum install -y memcached
-echo "Installing RabbitMQ"
+echo "Installing RabbitMQ..."
 yum install -y rabbitmq-server
-echo "Installing MongoDB"
+echo "Installing MongoDB..."
 yum install -y mongodb
